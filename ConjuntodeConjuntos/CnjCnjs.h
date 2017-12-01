c321d62a8809e0d98de1ce9c27ccb8115b05949d
Isaac Tretta <32001785+IsaacZero@users.noreply.github.com>
2017-11-30 20:07:31 -0600

    No entiendo :v

--- ConjuntodeConjuntos/CnjCnjs.h
+++ ConjuntodeConjuntos/CnjCnjs.h
@@ -8,6 +8,14 @@ template < typename V>
 class CnjCnjs{
 public: 
     
+    CnjCnjs();
+    
+    CnjCnjs(V elemento, string id);
+    
+    CnjCnjs(const CnjCnjs<V>& orig);
+    
+    virtual ~CnjCnjs();
+    
     //EFE: Inicializa el Conjunto de conjuntos.
     //REQ:
     //MOD:
@@ -29,40 +37,102 @@ public:
     bool vacio();
     
     //EFE: Retorna el identificador del conjunto donde se encuentra el elemento.
-    //REQ: El Conjunto inicializado y no vacio.
+    //REQ: El Conjunto inicializado y no vacio, y no elementos repetidos.
     //MOD:
     string cnjAlQuePertenece(V elemento);
     
     //EFE: Retorna true si el conjunto esta vacío, false si tiene al menos un elemento.
-    //REQ: El Conjunto inicializado.
+    //REQ: El Conjunto inicializado y no elementos repetidos.
     //MOD:
     void agregarConjunto(V elemento, string id);
     
+    //EFE: Une dos conjuntos.
+    //REQ: Ambos conjuntos inicializados y no vacios.
+    //MOD: El primer conjunto.
     void unirConjuntos(string cnjUno, string cnjDos);
+    
 private:
     
-    V* primerElemento;
-    CnjCnjs* sigConjunto;
+    V* primerElemento = 0;
+    V* sigElemento = 0;
+    CnjCnjs* sigConjunto = 0;
     string identificador;
+    static V* elementoNulo;
 };
 
-template < typename V >
-CnjCnjs::crear(){
+V* CnjCnjs<V>::elementoNulo = 0;
 
+template < typename V >
+CnjCnjs<V>::CnjCnjs(){
 }
 
 template < typename V >
-CnjCnjs::destruir(){
+CnjCnjs<V>::CnjCnjs(V* elemento, string id){
+    identificador = id;
+    primerElemento = elemento;
+}
+
+template < typename V >
+CnjCnjs<V>::CnjCnjs(const CnjCnjs<V>& orig){   
+}
+
+template < typename V >
+CnjCnjs<V>::~CnjCnjs(){
     
 }
 
 template < typename V >
-CnjCnjs::vaciar(){
+void CnjCnjs<V>::crear(){
+    primerElemento = elementoNulo;
+    identificador = "Contenedor";
+}
+
+template < typename V >
+void CnjCnjs<V>::destruir(){
     
 }
 
 template < typename V >
-CnjCnjs::vacio(){
+void CnjCnjs<V>::vaciar(){
+    
+}
+
+template < typename V >
+bool CnjCnjs<V>::vacio(){
+    bool empty = false;
+    if (primerElemento == elementoNulo)
+        empty = true;
+    return empty;
+}
+
+template < typename V >
+string CnjCnjs<V>::cnjAlQuePertenece(V* elemento){
+    string id = "";
+    bool encontrado = false;
+    if(primerElemento == elemento)
+        id = identificador;
+    else {
+        V* iterElem = sigElemento;
+        while(sigConjunto != 0 && !encontrado){
+            while(sigElemento != 0 && !encontrado){
+                if(iterElem)
+            }
+        }
+    }
+    return id;
+}
+
+template < typename V >
+void CnjCnjs<V>::agregarConjunto(V elemento, string id){
+    CnjCnjs<V>* newConj = new CnjCnjs(elemento, id);
+    if(primerElemento == elementoNulo)
+        primerElemento = newConj;
+    else
+        sigElemento = newConj;
+}
+
+template < typename V >
+void CnjCnjs<V>::unirConjuntos(string cnjUno, string cnjDos){
     
 }
 #endif /* CNJCNJS_H */
